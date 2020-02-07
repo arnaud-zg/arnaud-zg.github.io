075322fc35a07103906db4c09e2e2cd7944bc5c0
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2020-02-08 00:57:56 +0100

    chore: add title and description on head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import {
+  userName,
+  descriptions,
+  currentLocation,
+} from '../features/common/data.json';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,6 +19,15 @@ class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1, user-scalable=no"
           />
+          <title>
+            {userName} | {descriptions[0]}
+          </title>
+          <meta content="Portfolio" name={userName} />
+          <meta
+            name="description"
+            content={`${userName}, ${descriptions[0]} in ${currentLocation}`}
+          />
+          0
           <link rel="stylesheet" href="assets/css/main.css" />
           <noscript>
             <link rel="stylesheet" href="assets/css/noscript.css" />
