36337e17440a50c54a7c6ac74dedc8e073593e47
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2020-02-08 20:39:16 +0100

    feat: add tailwindcss

--- features/presentation/Presentation.tsx
+++ features/presentation/Presentation.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { SocialNetworks } from './socialNetworks';
 import { firstName, lastName, descriptions } from '../common/data.json';
+import classNames from 'classnames';
 
 export const Presentation = () => (
   <header id="header">
     <div>
-      <h1>
+      <h1 className={classNames('font-black', 'tracking-tight')}>
         {firstName} {lastName}
       </h1>
       <p>{descriptions.join(' • ')}</p>
