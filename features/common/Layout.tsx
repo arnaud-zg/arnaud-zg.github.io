299638e000d0d62db87f7a36c8e83d8f4914b08b
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2020-02-27 21:59:00 +0100

    fix: migrate #wrapper styles into tailwind

--- features/common/Layout.tsx
+++ features/common/Layout.tsx
@@ -1,5 +1,19 @@
 import { FC } from 'react';
+import classNames from 'classnames';
 
 export const Layout: FC = ({ children }) => {
-  return <div id="wrapper">{children}</div>;
+  return (
+    <div
+      className={classNames(
+        'h-full',
+        'left-0',
+        'opacity-0',
+        'fixed',
+        'top-0',
+        'w-full'
+      )}
+      style={{ animation: 'wrapper 3s forwards' }}>
+      {children}
+    </div>
+  );
 };
