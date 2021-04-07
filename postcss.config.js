76ab4731b0b6c543b5c4e6b47b2da796a686e907
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2021-04-07 16:00:27 +0200

    fix: use autoprefixer and postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-const purgeCss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    content: ['./pages/**/*.{ts,tsx}', './features/**/*.{ts,tsx}'],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
-
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    ...(process.env.NODE_ENV === 'production' ? [purgeCss] : []),
-    'postcss-preset-env',
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{ts,tsx}', './features/**/*.{ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
