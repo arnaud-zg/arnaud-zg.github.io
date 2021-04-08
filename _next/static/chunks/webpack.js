36e45137c8a542d4726ba973b251227cd94abeef
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2021-04-08 21:34:06 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0f5b0d8b821fe3c16a21";
+/******/ 	var hotCurrentHash = "4d2faef2452f62004e3f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
