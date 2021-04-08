e0f1dc4ce33f3c658fe5214d1c8b3c168547ebd8
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2021-04-08 17:53:46 +0200

    fix: add body component

---
+++ features/common/Body.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import classNames from 'classnames';
+
+interface BodyProps {}
+
+export const Body: FC<BodyProps> = ({ children }) => {
+  return (
+    <div
+      className={classNames(
+        'fixed',
+        'flex',
+        'h-full',
+        'inset-0',
+        'justify-center',
+        'text-black',
+        'w-full'
+      )}>
+      <div
+        className={classNames(
+          'bg-gray-200',
+          'h-auto',
+          'inline-block',
+          'my-6',
+          'overflow-y-scroll',
+          'p-4',
+          'w-4/5',
+          'rounded-lg'
+        )}>
+        {children}
+      </div>
+    </div>
+  );
+};
