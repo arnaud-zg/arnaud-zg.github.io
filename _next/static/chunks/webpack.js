2869eeb00a718b7c522de18ac74b666ea553191a
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2021-04-08 22:00:14 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4d2faef2452f62004e3f";
+/******/ 	var hotCurrentHash = "e14060fe3f2e0b913f67";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/pages/_app.127b51640f435a5fbc3d.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate_N_E("pages/_app",{
+
+/***/ "./public/assets/css/images/overlay.svg":
+/*!**********************************************!*\
+  !*** ./public/assets/css/images/overlay.svg ***!
+  \**********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"static/media/overlay.078a474672c3a8f67d6af17bc9198d52.svg\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcHVibGljL2Fzc2V0cy9jc3MvaW1hZ2VzL292ZXJsYXkuc3ZnP2JjZTIiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFBZSxvRkFBdUIsOERBQThEIiwiZmlsZSI6Ii4vcHVibGljL2Fzc2V0cy9jc3MvaW1hZ2VzL292ZXJsYXkuc3ZnLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgX193ZWJwYWNrX3B1YmxpY19wYXRoX18gKyBcInN0YXRpYy9tZWRpYS9vdmVybGF5LjA3OGE0NzQ2NzJjM2E4ZjY3ZDZhZjE3YmM5MTk4ZDUyLnN2Z1wiOyJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./public/assets/css/images/overlay.svg\n");
+
+/***/ })
+
+})
